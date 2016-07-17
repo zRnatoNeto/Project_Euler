9a11dc56e0942a1e562ae162eb4fa937136793b4
Renato Neto <Renato Neto>
2016-07-16 23:20:36 -0300

    Problem 1 solved

---
+++ Problem 1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumMulti = 0;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                    sumMulti += i;
+            }
+
+            Console.Write(sumMulti);
+            Console.ReadKey();
+        }
+    }
+}
