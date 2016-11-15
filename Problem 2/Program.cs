c06df08fffc5dbd24d0f2e13de1132ec5ef0af5c
RnatoNeto <RnatoNeto@RNATO-NOTE>
2016-11-15 03:08:53 -0200

    Problem 2 added

---
+++ Problem 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Problem_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sumFibonacci = 0;
+            int numA = 1, numB = 2, numC = 0;
+            while(sumFibonacci <= 4000000)
+            {
+                numC = numA + numB;
+                sumFibonacci += numA;
+                numA = numB;
+                numB = numC;
+            }
+            Console.Write(sumFibonacci);
+            Console.ReadKey();
+        }
+    }
+}
